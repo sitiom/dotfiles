5614e28398cff4f7867132dc40b824c40f477536
sitiom <56180050+sitiom@users.noreply.github.com>
2020-11-18 16:26:16 +0800

    Update uninstall-windows.ps1

--- uninstall-windows.ps1
+++ uninstall-windows.ps1
@@ -2,7 +2,7 @@ $CONFIG = "install-windows.conf.yaml"
 
 $confObj = ConvertFrom-Yaml ([string](Get-Content $CONFIG -Raw))
 foreach ($target in ($confObj | Where-Object Keys -eq link).Values.Keys) {
-    if ((Test-Path $target) -and (Get-Item $target).Attributes -match "ReparsePoint") {
+    if ((Get-Item $target -Force -ErrorAction SilentlyContinue).LinkType -eq "SymbolicLink") {
         Write-Host "Removing $target" -ForegroundColor Red
         Remove-Item $target
     }
