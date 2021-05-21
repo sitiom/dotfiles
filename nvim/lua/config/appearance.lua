64ffa85e927bc1e5a7560f2f616f6080722ee13a
Ryan Caezar Itang <sitiom@disroot.org>
2021-05-21 19:31:32 +0800

    Show list and add listchars

--- nvim/lua/config/appearance.lua
+++ nvim/lua/config/appearance.lua
@@ -1,4 +1,11 @@
 vim.o.termguicolors = true
 vim.o.guifont = 'Delugia Nerd Font:' .. (vim.fn.exists('g:started_by_firenvim') ~= 1 and "h15" or "h10")
 vim.cmd 'colorscheme codedark'
-
+vim.wo.list = true
+local listchars = {
+  'tab:» ',
+  'eol:↲',
+  'nbsp:␣',
+  'trail:•',
+}
+vim.o.listchars = table.concat(listchars, ',')
