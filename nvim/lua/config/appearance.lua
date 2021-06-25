b5a2b2f0337c0eb312bda9ff2b8eeebe61f0b1e5
Ryan Caezar Itang <sitiom@disroot.org>
2021-06-25 11:18:43 +0800

    Fix vscode.nvim breaking change

--- nvim/lua/config/appearance.lua
+++ nvim/lua/config/appearance.lua
@@ -1,6 +1,6 @@
 vim.o.termguicolors = true
 vim.o.guifont = 'Delugia Nerd Font:' .. (vim.fn.exists('g:started_by_firenvim') ~= 1 and "h15" or "h10")
-vim.o.background = 'dark'
+vim.g.vscode_style = "dark"
 vim.cmd 'colorscheme vscode'
 vim.wo.list = true
 local listchars = {
