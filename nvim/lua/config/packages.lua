a1cd79812b1a22f976bf13153c620d899f9b1a5f
Ryan Caezar Itang <sitiom@disroot.org>
2021-10-27 10:09:52 +0800

    Refactor packages.lua

--- nvim/lua/config/packages.lua
+++ nvim/lua/config/packages.lua
@@ -1,17 +1,14 @@
 -- Bootstrap packer.nvim
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.api.nvim_command 'packadd packer.nvim'
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
-
-vim.cmd 'packadd packer.nvim'
 
 -- Packages
 require('packer').startup(function()
   -- Self manage
-  use { 'wbthomason/packer.nvim', opt = true }
+  use 'wbthomason/packer.nvim'
 
   -- Vimpeccable
   use { 'svermeulen/vimpeccable', opt = true }
@@ -52,10 +49,9 @@ require('packer').startup(function()
     config = function() require('gitsigns').setup() end,
     requires = 'nvim-lua/plenary.nvim'
   }
-  use 'iwataka/cowsay.vim'
   use {
     'goolord/alpha-nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
+    requires = { 'kyazdani42/nvim-web-devicons', 'iwataka/cowsay.vim' },
     config = function ()
       local startify = require'alpha.themes.startify'
 
@@ -170,8 +166,6 @@ require('packer').startup(function()
           ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
           ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
           ['<CR>'] = cmp.mapping.confirm({
@@ -214,26 +208,28 @@ require('packer').startup(function()
       -- Use an on_attach function to only map the following keys 
       -- after the language server attaches to the current buffer
       local on_attach = function(client, bufnr)
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
         -- Mappings.
-        local opts = { noremap=true, silent=true }
-        buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        vimp.nnoremap({'silent'}, 'gh', function() require'lspsaga.provider'.lsp_finder() end)
-        vimp.nnoremap({'silent'}, '<leader>ca', function() require'lspsaga.codeaction'.code_action() end)
-        vimp.vnoremap({'silent'}, '<leader>ca', function() require'lspsaga.codeaction'.range_code_action() end)
-        vimp.nnoremap({'silent'}, 'K', function() require'lspsaga.hover'.render_hover_doc() end)
-        -- vimp.nnoremap({'silent'}, '<C-f>', function() require'lspsaga.action'.smart_scroll_with_saga(1) end)
-        -- vimp.nnoremap({'silent'}, '<C-b>', function() require'lspsaga.action'.smart_scroll_with_saga(-1) end)
-        vimp.nnoremap({'silent'}, 'gs', function() require'lspsaga.signaturehelp'.signature_help() end)
-        vimp.nnoremap({'silent'}, 'gr', function() require'lspsaga.rename'.rename() end)
-        -- vimp.nnoremap({'silent'}, 'gd', function() require'lspsaga.provider'.preview_definition() end)
-        vimp.nnoremap({'silent'}, '<leader>cd', function() require'lspsaga.diagnostic'.show_line_diagnostics() end)
-        vimp.nnoremap({'silent'}, '<leader>cc', function() require'lspsaga.diagnostic'.show_cursor_diagnostics() end)
-        vimp.nnoremap({'silent'}, '[d', function() require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev() end)
-        vimp.nnoremap({'silent'}, ']d', function() require'lspsaga.diagnostic'.lsp_jump_diagnostic_next() end)
+        vimp.add_buffer_maps(bufnr, function()
+          vimp.nnoremap({'silent'}, 'gD', function() vim.lsp.buf.declaration() end)
+          vimp.nnoremap({'silent'}, 'gi', function() vim.lsp.buf.implementation() end)
+          vimp.nnoremap({'silent'}, '<leader>D', function() vim.lsp.buf.type_definition() end)
+
+          -- lspsaga
+          vimp.nnoremap({'silent'}, 'gh', function() require'lspsaga.provider'.lsp_finder() end)
+          vimp.nnoremap({'silent'}, '<leader>ca', function() require'lspsaga.codeaction'.code_action() end)
+          vimp.vnoremap({'silent'}, '<leader>ca', function() require'lspsaga.codeaction'.range_code_action() end)
+          vimp.nnoremap({'silent'}, 'K', function() require'lspsaga.hover'.render_hover_doc() end)
+          vimp.nnoremap({'silent'}, '<C-f>', function() require'lspsaga.action'.smart_scroll_with_saga(1) end)
+          vimp.nnoremap({'silent'}, '<C-b>', function() require'lspsaga.action'.smart_scroll_with_saga(-1) end)
+          vimp.nnoremap({'silent'}, 'gs', function() require'lspsaga.signaturehelp'.signature_help() end)
+          vimp.nnoremap({'silent'}, 'gr', function() require'lspsaga.rename'.rename() end)
+          vimp.nnoremap({'silent'}, 'gd', function() require'lspsaga.provider'.preview_definition() end)
+          vimp.nnoremap({'silent'}, '<leader>cd', function() require'lspsaga.diagnostic'.show_line_diagnostics() end)
+          vimp.nnoremap({'silent'}, '<leader>cc', function() require'lspsaga.diagnostic'.show_cursor_diagnostics() end)
+          vimp.nnoremap({'silent'}, '[d', function() require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev() end)
+          vimp.nnoremap({'silent'}, ']d', function() require'lspsaga.diagnostic'.lsp_jump_diagnostic_next() end)
+        end)
+
         print'LSP Started.'
       end
 
@@ -249,26 +245,7 @@ require('packer').startup(function()
     end
   }
   use 'folke/lsp-colors.nvim'
-  use {
-    'simrat39/symbols-outline.nvim',
-    config = function()
-      vim.g.symbols_outline = {
-        highlight_hovered_item = true,
-        show_guides = true,
-        auto_preview = false, -- experimental
-        position = 'right',
-        keymaps = {
-          close = "<Esc>",
-          goto_location = "<Cr>",
-          focus_location = "o",
-          hover_symbol = "<C-space>",
-          rename_symbol = "r",
-          code_actions = "a"
-        },
-        lsp_blacklist = {}
-      }
-    end
-  }
+  use 'simrat39/symbols-outline.nvim' 
 
   -- Formatting
   use {
@@ -298,6 +275,7 @@ require('packer').startup(function()
   use 'editorconfig/editorconfig-vim'
   use {
     'ptzz/lf.vim',
+    after = 'vim-floaterm',
     config = function()
       vim.g.lf_map_keys = 0
       vim.g.lf_replace_netrw = 1
@@ -330,8 +308,7 @@ require('packer').startup(function()
       vimp.tnoremap({'silent'}, '<F9>', '<C-\\><C-n>:FloatermNext<CR>')
       vimp.nnoremap({'silent'}, '<F12>', ':FloatermToggle<CR>')
       vimp.tnoremap({'silent'}, '<F12>', '<C-\\><C-n>:FloatermToggle<CR>')
-    end,
-    opt = true
+    end
   }
   use {
     'andweeb/presence.nvim',
@@ -354,7 +331,11 @@ require('packer').startup(function()
 
   -- Misc
   use 'ThePrimeagen/vim-be-good'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 vim.cmd 'packadd vimpeccable'
-vim.cmd 'packadd vim-floaterm'
