44fd066314528188e54b4ed5fef6650daeb57748
Ryan Caezar Itang <sitiom@disroot.org>
2022-02-17 23:01:56 +0800

    Initial migration to chezmoi

--- nvim/lua/custom/autocmds.lua
+++
@@ -1,2 +0,0 @@
--- Autogroups
-vim.cmd "au TextYankPost * silent! lua vim.highlight.on_yank()"
--- nvim/lua/custom/options.lua
+++
@@ -1,9 +0,0 @@
-vim.o.guifont = 'Delugia Nerd Font:h10'
-vim.wo.list = true
-local listchars = {
-  'tab:» ',
-  'eol:↲',
-  'nbsp:␣',
-  'trail:•',
-}
-vim.o.listchars = table.concat(listchars, ',')
