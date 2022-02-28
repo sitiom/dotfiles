3da4f5aa15b1dfff21f37056e8d6938cbb8e566b
Ryan Caezar Itang <sitiom@disroot.org>
2022-02-28 18:46:13 +0800

    Dot source PowerShell scripts
    I was sourcing it the wrong way all this time

--- chezmoi/.chezmoitemplates/PowerShell/Completions/gh.ps1
+++ chezmoi/.chezmoitemplates/PowerShell/Completions/gh.ps1
@@ -1,3 +1,1 @@
-gh completion -s powershell | Join-String {
-    $_ -replace " ''\)$"," ' ')" -replace "(^\s+\)\s-join\s';')",'$1 -replace ";$wordToComplete$"' -replace "(\[CompletionResult\]::new\('[\w-]+)'",'$1 '''
-} -Separator "`n" | Invoke-Expression
+Invoke-Expression -Command $(gh completion -s powershell | Out-String)
--- chezmoi/.chezmoitemplates/PowerShell/Microsoft.PowerShell_profile.ps1
+++ chezmoi/.chezmoitemplates/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,8 @@
-& $PSScriptRoot\Microsoft.PowerShell_environment.ps1
+. $PSScriptRoot\Microsoft.PowerShell_environment.ps1
 
-Get-ChildItem $PSScriptRoot\Imports | ForEach-Object { & $_.FullName }
-Get-ChildItem $PSScriptRoot\Completions | ForEach-Object { & $_.FullName }
-Get-ChildItem $PSScriptRoot\Keybindings | ForEach-Object { & $_.FullName }
-
-# Initialize zoxide in $profile. Why does it not work when sourced? I don't know.
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
-
+Get-ChildItem $PSScriptRoot\Imports
+    | ForEach-Object { . $_ }
+Get-ChildItem $PSScriptRoot\Completions
+    | ForEach-Object { . $_ }
+Get-ChildItem $PSScriptRoot\Keybindings
+    | ForEach-Object { . $_ }
