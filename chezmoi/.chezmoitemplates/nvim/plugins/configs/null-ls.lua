649317fb22166461911e99f450839c52fb27afec
Ryan Caezar Itang <sitiom@disroot.org>
2022-07-02 11:24:06 +0800

    feat(vscode): add stylua extension

--- chezmoi/.chezmoitemplates/nvim/plugins/configs/null-ls.lua
+++ chezmoi/.chezmoitemplates/nvim/plugins/configs/null-ls.lua
@@ -2,19 +2,19 @@ local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
-  b.formatting.prettierd,
-  b.formatting.stylua,
-  b.formatting.shfmt,
+   b.formatting.prettierd,
+   b.formatting.stylua,
+   b.formatting.shfmt,
 
-  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
 local M = {}
 
 M.setup = function()
-  null_ls.setup {
-     sources = sources,
-  }
+   null_ls.setup {
+      sources = sources,
+   }
 end
 
 return M
