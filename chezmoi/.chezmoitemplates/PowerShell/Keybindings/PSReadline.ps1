a175b2ff0ca711e81e89240b7d3bcef01cbd454e
Ryan Caezar Itang <sitiom@proton.me>
2022-08-09 19:50:13 +0800

    chore(powershell): add PsFzf Scoop alias

---
+++ chezmoi/.chezmoitemplates/PowerShell/Keybindings/PSReadline.ps1
@@ -0,0 +1,7 @@
+Set-PSReadLineKeyHandler -Key Ctrl+Spacebar -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
--- chezmoi/.chezmoitemplates/PowerShell/Keybindings/PsFzf.ps1
+++ chezmoi/.chezmoitemplates/PowerShell/Keybindings/PsFzf.ps1
@@ -1,8 +1,2 @@
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-Set-PSReadLineKeyHandler -Key Ctrl+Spacebar -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-}
-
+Set-PsFzfOption -EnableAliasFuzzyScoop
