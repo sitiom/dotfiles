1a5a24c9f4ab1b15deb3d78fde1b74ced40d8203
Ryan Caezar Itang <sitiom@proton.me>
2024-04-28 00:51:14 +0800

    feat!: remove neovim config
    migrate to lazyvim

--- chezmoi/.chezmoitemplates/nvim/plugins/community.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.scrolling.satellite-nvim" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.syntax.hlargs-nvim" },
-  { import = "astrocommunity.utility.neodim" },
-}
